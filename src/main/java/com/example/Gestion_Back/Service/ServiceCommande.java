cc0a0928d70d318415d170d6a527ee8f5490cce4
ndieric <99005932+ndieric@users.noreply.github.com>
2022-09-13 17:22:52 +0200

    second commit

--- src/main/java/com/example/Gestion_Back/Service/ServiceCommande.java
+++ src/main/java/com/example/Gestion_Back/Service/ServiceCommande.java
@@ -3,7 +3,9 @@ package com.example.Gestion_Back.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.Gestion_Back.Dto.ProduitDto;
 import com.example.Gestion_Back.Models.Commande;
+import com.example.Gestion_Back.Models.Produit;
 import com.example.Gestion_Back.Repository.CommandeRepository;
 
 @Service
@@ -12,7 +14,21 @@ public class ServiceCommande {
     @Autowired
     CommandeRepository commandeRepository;
 
+
+
+
+   private Produit pro = new Produit();
+   private Commande com = new Commande();
+
+
+    
+   
+
     public Commande ajouter(Commande commande){
+        double pri = pro.getPrix();
+        double qua = com.getQuantite();
+        double tot = pri * qua;
+        commande.setMontant(tot);
         return commandeRepository.save(commande);
     }
 }
